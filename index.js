c11c967d28a22be3ab771ee9441d794815eede06
Samrat2814 <aslamhossainsamrat@gmail.com>
2024-12-07 20:00:08 +0600

    history section done

--- index.js
+++ index.js
@@ -24,12 +24,23 @@ document.getElementById("1").addEventListener("click", function () {
   // Update the donation amount
   const newAmount = currentAmount + donateInput;
   donateAmountElement.innerText = newAmount.toFixed(2);
+  console.log(newAmount);
 
   // Calculate the remaining amount
   remainingAmount -= donateInput;
   remainingAmountElement.innerText = remainingAmount.toFixed(2);
-  donateInputField.value = ""; 
+  donateInputField.value = "";
 
+  const historyItem = document.createElement("div");
+  historyItem.className = "border border-gray-400 p-6 mt-6 rounded-xl";
+  const currentDate = new Date().toString();
+  historyItem.innerHTML = `
+   <h1>${donateInput} Taka is Donated for famine-2024 at Noakhali, Bangladesh</h1>
+        <h1>Date : ${currentDate}</h1>
+  `;
+
+  const historyContainer = document.getElementById("20");
+  historyContainer.insertBefore(historyItem, historyContainer.firstChild);
   document.getElementById("my_modal_5").showModal();
 });
 
@@ -63,7 +74,18 @@ document.getElementById("5").addEventListener("click", function () {
   // Calculate the remaining amount
   remainingAmount -= donateInput;
   remainingAmountElement.innerText = remainingAmount.toFixed(2);
-  donateInputField.value = ""; 
+  donateInputField.value = "";
+
+  const historyItem = document.createElement("div");
+  historyItem.className = "border border-gray-400 p-6 mt-6 rounded-xl";
+  const currentDate = new Date().toString();
+  historyItem.innerHTML = `
+   <h1>${donateInput} Taka is Donated for famine-2024 at Feni, Bangladesh</h1>
+        <h1>Date : ${currentDate}</h1>
+  `;
+
+  const historyContainer = document.getElementById("20");
+  historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 
   document.getElementById("my_modal_5").showModal();
 });
@@ -100,5 +122,34 @@ document.getElementById("8").addEventListener("click", function () {
   remainingAmountElement.innerText = remainingAmount.toFixed(2);
   donateInputField.value = "";
 
+  const historyItem = document.createElement("div");
+  historyItem.className = "border border-gray-400 p-6 mt-6 rounded-xl";
+  const currentDate = new Date().toString();
+  historyItem.innerHTML = `
+   <h1>${donateInput} Taka is Donated for Quota movement at Dhaka, Bangladesh</h1>
+        <h1>Date : ${currentDate}</h1>
+  `;
+
+  const historyContainer = document.getElementById("20");
+  historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
   document.getElementById("my_modal_5").showModal();
 });
+
+document.getElementById("11").addEventListener("click", function () {
+  const hiddenAdd = document.getElementById("13");
+  hiddenAdd.classList.add("hidden");
+  this.style.backgroundColor = "#B4F461";
+  const donationBtn = document.getElementById("12");
+  donationBtn.style.backgroundColor = "#F4F4F4";
+});
+
+document.getElementById("12").addEventListener("click", function () {
+  const hiddenAdd = document.getElementById("13");
+  hiddenAdd.classList.remove("hidden");
+  (this.style.backgroundColor = "#B4F461"), "border";
+  const donationBtn = document.getElementById("11");
+  donationBtn.style.backgroundColor = "#F4F4F4";
+});
+
+
